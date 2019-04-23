a7796382c4d5a6c25f927cb2d915e45f8b1d79c9
Julian V <jvenci@comcast.net>
2019-04-23 16:19:05 -0700

    Implement basic casssandra db files

---
+++ db-cassandra/index.js
@@ -0,0 +1,58 @@
+const ExpressCassandra = require('express-cassandra');
+
+const models = ExpressCassandra.createClient({
+  clientOptions: {
+    contactPoints: ['127.0.0.1'],
+    protocolOptions: { port: 9042 },
+    keyspace: 'mykeyspace',
+    queryOptions: { consistency: ExpressCassandra.consistencies.one },
+  },
+  ormOptions: {
+    defaultReplicationStrategy: {
+      class: 'SimpleStrategy',
+      replication_factor: 1,
+    },
+    migration: 'safe',
+  },
+});
+
+const Stock = models.loadSchema('stock', {
+  fields: {
+    ask_price: 'float',
+    ask_size: 'int',
+    bid_price: 'float',
+    bid_size: 'int',
+    last_extended_hours_trade_price: 'float',
+    last_trade_price: 'float',
+    symbol: 'varchar',
+    quantity: 'int',
+  },
+  key: ['symbol'],
+});
+
+const Account = models.loadSchema('account', {
+  fields: {
+    account_number: 'varchar',
+    buying_power: 'float',
+    option_level: 'int',
+    watch_list: 'varchar',
+  },
+  key: ['account_number'],
+});
+
+Stock.syncDBAsync()
+  .then(() => console.log ('Stocks synchronized'))
+  .then(() => Account.syncDBAsync())
+  .then(() => console.log('Account synchornized'))
+  .catch(err => console.error('unable to synchornize : ' + err)) 
+
+
+// MyModel or models.instance.Person can now be used as the model instance
+//console.log(models.instance.Stock === Stocks);
+
+// sync the schema definition with the cassandra database table
+// if the schema has not changed, the callback will fire immediately
+// otherwise express-cassandra will try to migrate the schema and fire the callback afterwards
+
+
+module.exports = models;
