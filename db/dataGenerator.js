7aa83466bd5696debc932879b7f1ae1bc49d1d25
Julian V <jvenci@comcast.net>
2019-04-30 17:00:31 -0700

    Delete commented out code

--- db/dataGenerator.js
+++ db/dataGenerator.js
@@ -33,36 +33,14 @@ const createMockDataCSV = async () => {
   }
 };
 
+const createCompressedMockDataCSV = async () => {
+  for (let i = 0; i < 10000001; i++) {
+    if (i === 0) {
+      console.log('ask_price, ask_size, bid_price, bid_size, last_extended_hours_trade_price, last_trade_price, symbol, quantity\n');
+    } else if (!console.log(`${faker.finance.amount()},${faker.random.number()},${faker.finance.amount()},${faker.random.number()},${faker.finance.amount()},${faker.finance.amount()},${tickers[i]}, ${faker.finance.amount()}\n`)) {
+        await new Promise(resolve => file.once('drain', resolve));
+    }
+  }
+
+
 createMockDataCSV();
-
-// const createMockDataJSON = async () => {
-//   for (let i = 0; i < 1e7; i++) {
-//     const stockData = {
-//       ask_price: faker.finance.amount(),
-//       ask_size: faker.random.number(),
-//       bid_price: faker.finance.amount(),
-//       bid_size: faker.random.number(),
-//       last_extended_hours_trade_price: faker.finance.amount(),
-//       last_trade_price: faker.finance.amount(),
-//       symbol: faker.random.alphaNumeric(5),
-//       createdAt: '2019-03-29T17:37:11.000Z',
-//       updatedAt: '2019-03-29T17:37:11.000Z',
-//     };
-//     if (i === 0) {
-//       file.write('[');
-//     }
-//     if (!file.write(JSON.stringify(stockData))) {
-//         await new Promise(resolve => file.once('drain', resolve));
-//     }
-//     if (i )
-//   }
-// }
-
-
-
-      // fs.createWriteStream('mockData.json', JSON.stringify(mockData), 'utf8', (err) => {
-  //   if (err) throw err;
-  //   console.log('file write successful!');
-  // });
-  // const file = await fs.readFile('mockData.json', 'utf8');
-  // await write(file, JSON.stringify(mockData));
